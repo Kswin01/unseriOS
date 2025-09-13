6366aa404cf89c62c70157bf8a1b2a5f1c455f41
Krishnan Winter <krishnan-git@wintermail.me>
2025-09-13 23:06:27 +1000

    cpu: enable the fpu
    Signed-off-by: Krishnan Winter <krishnan-git@wintermail.me>

--- kernel/src/cpu.c
+++ kernel/src/cpu.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <util.h>
 
 #include <cpu.h>
 
+/* CPACR_EL1 register */
+#define CPACR_EL1_FPEN          20     // FP registers access
+
 void init_cpu() {
     puts("Initialising the CPU!\n");
+    /* Init the FPU */
+    uint32_t cpacr;
+    MRS("cpacr_el1", cpacr);
+    cpacr |= (3 << CPACR_EL1_FPEN);
+    MSR("cpacr_el1", cpacr);
+    asm volatile("isb");
 }
